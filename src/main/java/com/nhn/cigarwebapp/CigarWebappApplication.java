e00e67e87a970e8907039692ac4091f0e86cb60d
hoangnam1909 <nguyenhoangn023@gmail.com>
2023-07-05 11:07:06 +0700

    first commit

---
+++ src/main/java/com/nhn/cigarwebapp/CigarWebappApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.cigarwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CigarWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CigarWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhn/cigarwebapp/CigarWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhn.cigarwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CigarWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
