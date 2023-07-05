8b67966d602241f7dd165dbc90df71016f8f251f
hoangnam1909 <nguyenhoangn023@gmail.com>
2023-07-05 11:08:19 +0700

    hello

--- src/main/java/com/nhn/cigarwebapp/CigarWebappApplication.java
+++ src/main/java/com/nhn/cigarwebapp/CigarWebappApplication.java
@@ -2,9 +2,17 @@ package com.nhn.cigarwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CigarWebappApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Azure!!!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CigarWebappApplication.class, args);
